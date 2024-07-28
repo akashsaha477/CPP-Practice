c2b68ff6d5a25107881d3dbb889ca219e64bee80
Akash Saha <akashsaha477@gmail.com>
2024-07-28 12:08:04 +0530

    switch.cpp

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name;
+    int age;
+    cout<<"Enter your name: ";
+    getline(cin>>ws,name);
+    cout<<"Enter your age: ";
+    cin>>age;
+    cout<<"Your name is "<<name<<" and your age is "<<age;
+}
+
+
